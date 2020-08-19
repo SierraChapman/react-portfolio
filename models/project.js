d676e69ff97095c93b40a27871bcf377b0d53c31
Sierra Chapman <siechap@gmail.com>
2020-08-18 22:14:45 -0700

    create project model

--- models/project.js
+++ models/project.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
-  // PROJECT FIELDS GO HERE
+  rank: Number, // ordor to display on page
+  title: String,
+  description: String,
+  image: String, // name of image file
+  url: String, // deployed link
+  repo: String, // link to GitHub repository
+  tags: [String], // skills/technologies used
 });
 
 const Project = mongoose.model("Project", projectSchema);
