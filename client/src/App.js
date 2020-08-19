0c0ffb720c894cb7231651fd613d89e437ab6d3b
Sierra Chapman <siechap@gmail.com>
2020-08-18 19:56:38 -0700

    set up basic react app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sierra Chapman</h1>
+      <h2>React Portfolio</h2>
+    </div>
+  );
+}
+
+export default App;
