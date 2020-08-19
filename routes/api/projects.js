90fd7ab59d4f4f28068fc47f30cff8f864396079
Sierra Chapman <siechap@gmail.com>
2020-08-18 21:26:09 -0700

    create dummy API route

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -2,5 +2,10 @@ const router = require("express").Router();
 const projectsController = require("../../controllers/projectsController");
 
 // API ROUTES GO HERE
+router
+  .route("/")
+  .get((req, res) => {
+    res.json([]);
+  });
 
 module.exports = router;
