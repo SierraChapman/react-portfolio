d587673f3375bca1910ed13eefa82ff0861ff8bf
Sierra Chapman <siechap@gmail.com>
2020-08-19 23:28:30 -0700

    create projects page

--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import "./style.css";
 
-function Project() {
-  return <h1>Project</h1>;
+function Project({ project }) {
+  return (
+    <section>
+      <h3>{project.title}</h3>
+      <img 
+        className="project-image" 
+        src={`${process.env.PUBLIC_URL}/assets/img/projects/${project.image}`} 
+        alt={project.title}
+      />
+
+      <h4>Description</h4>
+      <p>{project.description}</p>
+
+      <h4>Links</h4>
+      <p><a href={project.url}>Deployed Application</a></p>
+      <p><a href={project.repo}>GitHub Repository</a></p>
+
+      <h4>Skills/Technologies Used</h4>
+      <ul>
+        {project.tags.map(tag => <li>{tag}</li>)}
+      </ul>
+
+    </section>
+  );
 }
 
 export default Project;
--- client/src/pages/Projects/index.js
+++ client/src/pages/Projects/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useProjectContext } from "../../utils/GlobalState";
+import Project from "../../components/Project";
 import "./style.css";
 
 function Projects() {
-  return <h1>Projects</h1>;
+  const state = useProjectContext()[0];
+
+  return (
+    <main id="projects">
+      <h2>Featured Projects</h2>
+      {state.projects.map(project => <Project project={project} key={project.repo}/>)}
+    </main>
+  );
 }
 
 export default Projects;
