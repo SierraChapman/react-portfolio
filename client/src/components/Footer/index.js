158cfbcec84f1472ffd3b6e0c98412cb83216489
Sierra Chapman <siechap@gmail.com>
2020-08-19 22:48:48 -0700

    create footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Container from "../Container";
 import "./style.css";
 
 function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <footer>
+      <Container>
+        <p>Created by Sierra Chapman</p>
+        <p>using MongoDB, Express, React, and Node.js</p>
+        <p><a href="https://github.com/SierraChapman/react-portfolio">View on GitHub</a></p>
+      </Container>
+    </footer>
+  );
 }
 
 export default Footer;
