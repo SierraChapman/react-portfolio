6f8a29e80d53271b624bdfc482639bb1b0a4f057
Sierra Chapman <siechap@gmail.com>
2020-08-19 22:27:45 -0700

    create about page

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import "./style.css";
 
 function About() {
-  return <h1>About</h1>;
+  return (
+    <main id="about">
+      <h2>About Me</h2>
+      <div>
+        <img className="portrait" src={`${process.env.PUBLIC_URL}/assets/img/portrait.jpeg`} alt="Sierra Chapman" />
+        <p>
+          I am a web development student located in Pleasanton, CA. 
+          I graduated B.S. in Physics from UC Davis in 2019, where I gained programming experience though using Python in astrophysics research. 
+          I am currently studying full-stack web development the UC Berkeley Coding Bootcamp. 
+          In my free time, I enjoy running, playing video games, and reading science fiction and fantasy.
+        </p>
+      </div>
+    </main>
+  );
 }
 
 export default About;
