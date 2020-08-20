61bfbc1676029460225dc751eb324f7c30bfa3d1
Sierra Chapman <siechap@gmail.com>
2020-08-19 23:39:41 -0700

    create contact info page

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import "./style.css";
 
 function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <main id="contact">
+      <h2>Contact Info</h2>
+      <ul>
+        <li><a href="https://github.com/SierraChapman">GitHub</a></li>
+        <li><a href="https://www.linkedin.com/in/sierra-chapman">LinkedIn</a></li>
+        <li><a href="mailto:siechap@gmail.com">Email</a></li>
+        <li><a href={`${process.env.PUBLIC_URL}/assets/pdf/resume.pdf`}>Resume</a></li>
+      </ul>
+    </main>
+  );
 }
 
 export default Contact;
