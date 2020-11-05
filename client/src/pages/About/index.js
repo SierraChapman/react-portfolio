bd3e3e0ffbcdd1441851d6197e672f9d99b8ca37
Sierra Chapman <siechap@gmail.com>
2020-11-05 12:03:59 -0800

    fix typo in bio

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -9,7 +9,7 @@ function About() {
         <img className="portrait" src={`${process.env.PUBLIC_URL}/assets/img/portrait.jpeg`} alt="Sierra Chapman" />
         <p>
           I am a full-stack web developer located in Pleasanton, CA and a recent graduate from the UC Berkeley Coding Bootcamp. 
-          Before going into web development, I earned a B.S. in Physics from UC Davis in 2019, where I gained programming experience though using Python for data analysis in astrophysics research. 
+          Before going into web development, I earned a B.S. in Physics from UC Davis in 2019, where I gained programming experience through using Python for data analysis in astrophysics research. 
           In my free time, I enjoy running, playing video games, and reading science fiction and fantasy.
         </p>
       </div>
